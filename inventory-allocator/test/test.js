7434d965450520314893904be8aee0a6b92061a3
Francis Distor <francis.distor@gmail.com>
2019-06-17 14:21:07 -0700

    add tests for searching warehouses and creating cheapest shipment

--- inventory-allocator/test/test.js
+++ inventory-allocator/test/test.js
@@ -39,4 +39,67 @@ describe("Inventory Allocator class", () => {
   });
 });
 
-describe("Inventory Allocator functionality", () => {});
+describe("Inventory Allocator functionality", () => {
+  let inventoryAllocator;
+
+  beforeEach(() => {
+    const order = {};
+
+    inventoryAllocator = new InventoryAllocator(warehouses, order);
+  });
+
+  it("Should handle an empty order", () => {
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([]);
+  });
+
+  it("Should handle an empty warehouse list", () => {
+    inventoryAllocator.warehouses = [];
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([]);
+  });
+
+  it("Should return no warehouses if items do not exist", () => {
+    inventoryAllocator.order = { pear: 1, strawberry: 1 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([]);
+  });
+
+  it("Should retrieve from one warehouse if all items are found", () => {
+    inventoryAllocator.order = { banana: 1 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([
+      { pol: { banana: 1 } }
+    ]);
+  });
+
+  it("Should retrieve from the first warehouse if all items are found", () => {
+    inventoryAllocator.order = { apple: 1 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([
+      { owd: { apple: 1 } }
+    ]);
+  });
+
+  it("Should retrieve from multiple warehouses if all items found", () => {
+    inventoryAllocator.order = { apple: 2, banana: 1, orange: 5 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([
+      { owd: { apple: 1 } },
+      { dmk: { apple: 1 } },
+      { pol: { banana: 1 } },
+      { ida: { orange: 5 } }
+    ]);
+  });
+
+  it("Should return no warehouses if insufficient inventory", () => {
+    inventoryAllocator.order = { orange: 6 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([]);
+  });
+
+  it("Should return no warehouses if not all items are found", () => {
+    inventoryAllocator.order = { apple: 1, banana: 1, orange: 6 };
+
+    expect(inventoryAllocator.searchWarehouses()).to.deep.equal([]);
+  });
+});

0dbea92f9566fc25cf1eb40e619aaf9034f05eb0
Francis Distor <francis.distor@gmail.com>
2019-06-17 13:56:35 -0700

    add test for non mutating properties

--- inventory-allocator/test/test.js
+++ inventory-allocator/test/test.js
@@ -8,7 +8,6 @@ describe("Inventory Allocator class", () => {
   beforeEach(() => {
     const inventory = [];
     const order = {};
-
     inventoryAllocator = new InventoryAllocator(inventory, order);
   });
 
@@ -21,7 +20,23 @@ describe("Inventory Allocator class", () => {
     expect(inventoryAllocator.order).to.deep.equal({});
   });
 
+  it("Should not mutate properties if inputs change", () => {
+    const order = { apple: 1 };
+    const warehouses = [{ name: "owd", inventory: { apple: 1 } }];
+    inventoryAllocator = new InventoryAllocator(warehouses, order);
+
+    order.apple = 10;
+    warehouses[0].name = "dm";
+
+    expect(inventoryAllocator.order).to.deep.equal({ apple: 1 });
+    expect(inventoryAllocator.warehouses).to.deep.equal([
+      { name: "owd", inventory: { apple: 1 } }
+    ]);
+  });
+
   it("Should have methods", () => {
     expect(typeof inventoryAllocator.searchWarehouses).to.equal("function");
   });
 });
+
+describe("Inventory Allocator functionality", () => {});

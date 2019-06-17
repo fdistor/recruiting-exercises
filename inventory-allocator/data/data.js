ed86227793d5d048c95bd01cbe5cc8427c764349
Francis Distor <francis.distor@gmail.com>
2019-06-17 14:00:39 -0700

    create mock warehouse inventories

---
+++ inventory-allocator/data/data.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { name: "owd", inventory: { apple: 1 } },
+  { name: "dmk", inventory: { apple: 1 } },
+  { name: "pol", inventory: { banana: 1 } },
+  { name: "ida", inventory: { orange: 5, apple: 10 } }
+];

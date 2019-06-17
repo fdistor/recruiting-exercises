5e5cf7fd32f0e4b2238c6673ae9db33d357667c2
Francis Distor <francis.distor@gmail.com>
2019-06-17 13:35:53 -0700

    add constructor and searchWarehouses function

---
+++ inventory-allocator/src/inventoryAllocator.js
@@ -0,0 +1,10 @@
+class InventoryAllocator {
+  constructor(warehouses, order) {
+    this.order = order;
+    this.warehouses = warehouses;
+  }
+
+  searchWarehouses() {}
+}
+
+module.exports = InventoryAllocator;
